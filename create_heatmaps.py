485e4c07983e56e00526397b36752f20ed20c5e9
Max Lu <39780468+fedshyvana@users.noreply.github.com>
2021-07-28 10:30:46 -0400

    Update create_heatmaps.py
    fix typo

--- create_heatmaps.py
+++ create_heatmaps.py
@@ -33,7 +33,7 @@ args = parser.parse_args()
 def infer_single_slide(model, features, label, reverse_label_dict, k=1):
 	features = features.to(device)
 	with torch.no_grad():
-		if isinstance(model, (CLAM_SB,)):
+		if isinstance(model, (CLAM_SB, CLAM_BM)):
 			model_results_dict = model(features)
 			logits, Y_prob, Y_hat, A, _ = model(features)
 			Y_hat = Y_hat.item()
