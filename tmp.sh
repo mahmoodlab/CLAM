3a095897b8b0b5256f96e37d0673b0d2217b2e17
haydenych <heiheiyu127@gmail.com>
2024-07-31 13:09:29 -0400

    temp

--- tmp.sh
+++
@@ -1,8 +0,0 @@
-python3 create_patches_fp_mag.py \
-    --source "/home/heiheiyu127/Documents/PMCC/slides/PKG - CPTAC-LUAD_v12/LUAD/" \
-    --save_dir "/home/heiheiyu127/Documents/PMCC/slides/processed/LUAD_mag/" \
-    --seg --patch --stitch \
-    --expected_mag_size 20 \
-    --expected_patch_size 224 \
-    --step_size_overlap 0 \
-    --process_list "/home/heiheiyu127/Documents/PMCC/slides/processed/LUAD_mag/process_list_mag.csv"
